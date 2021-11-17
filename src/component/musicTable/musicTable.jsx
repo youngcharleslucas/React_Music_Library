475a030123bf7a1b2174bee0c9ac792569c7ec1c
Your Name <charles.luas.young@gmail.com>
2021-11-17 12:09:23 -0700

    Return table

---
+++ src/component/musicTable/musicTable.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class MusicTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+           <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Song Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Genre</th>
+                    <th>Release Date</th>
+                </tr>
+                <tr>
+                    <td>{this.state.id}</td>
+                    <td>{this.state.title}</td>
+                    <td>{this.state.artist}</td>
+                    <td>{this.state.album}</td>
+                    <td>{this.state.genre}</td>
+                    <td>{this.state.release_date}</td>
+                </tr>
+            </table>
+         );
+    }
+}
+ 
+export default MusicTable;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
+import axios from 'axios';
 
 
 ReactDOM.render(<App />,document.getElementById('root'));
